d5bb9077a599e71b40e5da41b561b796003db2e9
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-05-01 11:29:17 -0600

    updated scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,36 @@
 alert('Hello World!')
+// grab out <section> element for ouput.
+const apiTestElement = document.getElementById( 'api-test' );
+
+/**
+ * make a request for all pets to the API 
+ */
+
+ // Set the request URL.
+ fetch('http://localhost:80/api/pets.php')
+ // convert the response from a string to an object.
+ .then( response => response.json() )
+ // Use the data (oject)!
+ .then (pets => {
+    const heading = document.createElement ( 'H2' );
+    heading.textContent = 'Pets';
+    const list = document.createElement( 'UL' );
+    for (const pet in pets){
+        const listItem = document.createElement ( 'LI' );
+        listItem.innerHTML = `
+        <dl>
+            <dt>Age</dt>
+            <dd>${pet.age}</dd>
+            <dt>Type</dt>
+            <dd>${pet.type}</dd>
+            </dl>
+            `;
+        const tricksList = document.createElement('UL');
+        for ( tricks of pets.tricks ){
+            tricksList.innerHTML += `<li>${trick}</li>`;
+        }
+        listItem.appendChild( trackList );
+        list.appendChild ( listItem );
+    }
+    apiTestElement.appendChild( list );
+ })
